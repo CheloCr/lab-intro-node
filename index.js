85d8034140196fb0b18c307c798fb1d719439947
CheloCr <xchell_cruz@hotmail.com>
2021-11-15 17:26:39 -0600

    terminado

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+   this.items = []
+   this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+    this.items.sort((a, b) => (a - b));
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    if (pos > this.length){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+    
+  }
 
-  min() {}
+  max() {
 
-  sum() {}
+    if(this.length===0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items.sort((a, b) => (a - b))[this.length - 1];
+    
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length===0){
+      throw new Error('EmptySortedList');
+    }
+    return this.items.sort((a, b) => (a - b))[0];
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0
+    }
+
+    let sumaArray = 0
+    this.items.forEach((numero => sumaArray += numero))
+    return sumaArray
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+
+    return this.items.reduce((a, b) => a + b) / this.length;
+
+
+  }
 }
 
 module.exports = SortedList;
